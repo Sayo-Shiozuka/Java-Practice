8f1d094ee366054bef0e5f066ba5ac666b93b06f
Sayo Fukuyama <sayosf1026@gmail.com>
2021-05-24 22:50:39 +0900

    [first commit]chapter0

---
+++ chapter0.java
@@ -0,0 +1,57 @@
+// 0-1 HelloWorld プログラム
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("hello, world!");
+	}
+}
+
+// 0-2 一部を書き換えたプログラム
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("すがわら");
+    }
+
+// 0-3 湊くんが作ったプログラム（エラー）　文字列の後に"記号がない部分の訂正
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("湊くんかっこいい！最高！");
+    }
+}
+
+// 0-4 複数の文章を表示する
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("すがわら");
+        System.out.println("31歳です");
+        System.out.println("お酒がすきです");
+    }
+}
+
+// 0-5 計算をする
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("すがわら");
+        System.out.println("31歳です");
+        System.out.println("お酒がすきです");
+        System.out.println("31 + 31の計算をします");
+        System.out.println(31 + 31);
+    }
+}
+
+// 0-6 変数を使ってみる
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("すがわら");
+        System.out.println("31歳です");
+        System.out.println("お酒がすきです");
+        System.out.println("31 + 31の計算をします");
+        System.out.println(31 + 31);
+        int x;
+        x = 6;
+        System.out.println(x * x * 3.14);
+    }
+}
+
+
+
+
