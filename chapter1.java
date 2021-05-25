d2e4354e75e31d319ee656fde9b61f9e8719334f
Sayo Fukuyama <sayosf1026@gmail.com>
2021-05-25 15:36:03 +0900

    [start]chapter1

---
+++ chapter1.java
@@ -0,0 +1,11 @@
+// 1-1 ソースコードの基本構造
+public class Main {　     //Mainはクラス名/ソースファイルは「クラス名.java」
+	public static void main(String[] args) {   //ここまでのコードは暗記する
+		System.out.println("RPG:スッキリ魔王征伐");
+		System.out.println("Ver.0.1 by 湊");
+		System.out.println("<ただいま鋭意学習・製作中>");
+		System.out.println("プログラムを終了します");
+	}
+}
+
+
