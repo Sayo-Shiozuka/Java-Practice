91447d5eae2ee5dbf27e46be6c171d6baccc0438
Sayo Fukuyama <sayosf1026@gmail.com>
2021-05-26 21:30:59 +0900

    [start]2章

---
+++ chapter2.java
@@ -0,0 +1,11 @@
+// 2-1 変数宣言の文と計算の文
+public class Main {
+  public static void main(String[] args){
+    int a;
+    int b;
+    a = 20;
+    b = a + 5;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
