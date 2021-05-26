6e8c0611cc91a0b7b7b7ad7f5e0bfb5731d5bf63
Sayo Fukuyama <sayosf1026@gmail.com>
2021-05-26 21:40:26 +0900

    2-2:オペランド

--- chapter2.java
+++ chapter2.java
@@ -9,3 +9,18 @@ public class Main {
     System.out.println(b);
   }
 }
+
+// 2-2 エスケープシーケンスを用いていない例（エラー）
+public class Main {
+  public static void main(String[] args){
+    System.out.println("私の好きな記号は二重引用符(")"); //文字列とみなされる部分が変になる
+  }
+}
+
+// 2-3 エスケープシーケンスを用いた例
+public class Main {
+  public static void main (String[] args){
+    System.out.println("私の好きな記号は二重引用符(¥")です"); //¥"によって二重引用符は文字とみなされる
+  }
+}
+
