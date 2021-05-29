0774af773f82c6d094400161b78a8d52053d13dc
Sayo Fukuyama <sayosf1026@gmail.com>
2021-05-29 23:59:33 +0900

    3-4:分岐構文のバリエーション

--- chapter3.java
+++ chapter3.java
@@ -33,3 +33,61 @@ public class Main {
     System.out.println("DVDを見ます")
   }
 }
+
+//3-4 冗長でスッキリしないソースコード
+public class Main {
+  public static void main(String[] args){
+    System.out.println("あなたの運勢を占います");
+    int fortune = new java.util.Random().nextInt(4) + 1
+    if (fortune == 1){
+      System.out.println("大吉");
+    }else if (fortune == 2){
+      System.out.println("中吉");
+    }else if (fortune == 3) {
+      System.out.println("吉");
+    }else {
+      System.out.println("凶");
+    }
+  }
+}
+
+//3-5 break文を忘れると...
+public class Main {
+  public static void main(String[] args){
+    System.out.println("あなたの運勢を占います");
+    int fortune = 1;
+    switch (fortune){
+      case 1:
+        system.out.println("大吉");
+        //ここにbreak;を入れ忘れている
+      case 2:
+        system.out.println("中吉");
+      break;
+      case 3:
+        system.out.println("吉");
+      break;
+      default:
+        system.out.println("凶");
+    }
+  }
+}
+
+//3-6 あえてbreak文を書かない
+public class Main{
+  public static void main(String[] args){
+    System.out.println("あなたの運勢を占います");
+    int fortune = new java.util.Random().nextInt(5) + 1;
+    switch (fortune) {
+      case 1:
+      case 2:     //fortuneが1か2なら
+        System.out.println("いいね！");
+        break;
+      case 3:
+        System.out.println("普通です");
+        break;
+      case 4:
+      case 5:
+        System.out.println("うーん...");
+    }
+  }
+}
