c0f30c2779f40292c3ac331cbe09d94c5b74c834
Sayo Fukuyama <sayosf1026@gmail.com>
2021-05-29 23:36:09 +0900

    3-2:ブロックの書き方

--- chapter3.java
+++ chapter3.java
@@ -21,3 +21,15 @@ public class Main {
     }
   }
 }
+
+//3-3 波カッコを省略した記述
+public class Main {
+  public static void main(String[] args){
+    boolean tenki = true;
+    if (tenki == true){   //内容が2行なので波カッコは省略不可能
+      System.out.println("洗濯をします");
+      System.out.println("散歩にいきます");
+    }else     //1行しかないので波カッコは省略可能
+    System.out.println("DVDを見ます")
+  }
+}
