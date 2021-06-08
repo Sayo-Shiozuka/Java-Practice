6033d292c7978bba76dc9238fad6b7bc9c72edd9
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-08 21:01:16 +0900

    4-12:DNAランダム表示

--- chapter4.java
+++ chapter4.java
@@ -113,3 +113,33 @@ public class Main {
     System.out.println("50点以上の科目の数は:" + count);
   }
 }
+
+4-12 DNAの記号をランダムに表示する
+public class Main{
+  public static void main(String[] args){
+    int[] seq = new int[10];
+
+    //塩基配列をランダムに生成
+    for (int i = 0; i < seq.length; i++){
+      seq[i] = new.java.util.Random().nextInt(4);
+    }
+
+    //生成した塩基配列の記号を表示
+    for (int i = 0; i < seq.length; i++){
+      switch (seq[i]){
+        case 0:
+          System.out.print("A ");
+          break;
+        case 1:
+          System.out.print("T ");
+          break;
+        case 2:
+          System.out.print("G ");
+          break;
+        case 3:
+          System.out.print("C ");
+          break;
+      }
+    }
+  }
+}
