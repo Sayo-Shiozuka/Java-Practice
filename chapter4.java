cf6604a01cf9b91c863458c6859022187fceb885
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-08 20:52:00 +0900

    4-11:カウント集計

--- chapter4.java
+++ chapter4.java
@@ -99,3 +99,17 @@ public class Main {
     System.out.println("平均点:" + avg);
   }
 }
+
+4-11 50点以上の科目の数を調べる
+public class Main {
+  public static void main(String[] args){
+    int[] scores = {20, 30, 40, 50, 80};
+    int count = 0;
+    for (int i = 0; i < scores.length; i++){
+      if (scores[i] >= 50){
+        count++;
+      }
+    }
+    System.out.println("50点以上の科目の数は:" + count);
+  }
+}
