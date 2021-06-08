317e6f8bd39cdb50c1dac4471db01292a3e40dfd
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-08 21:04:57 +0900

    4-13:従来for文

--- chapter4.java
+++ chapter4.java
@@ -143,3 +143,13 @@ public class Main{
     }
   }
 }
+
+4-13 従来のfor文
+public class Main {
+  public static void main(String[] args){
+    int[] scores = {20, 30, 40, 50, 80};
+    for (int i = 0; i < scores.length; i++){
+      System.out.println(scores[i]);
+    }
+  }
+}
