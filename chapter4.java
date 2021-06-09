ae0c6061a8798cb8c1f6c18a000f26e23ef184b6
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-09 21:08:24 +0900

    4-15:配列を理解する

--- chapter4.java
+++ chapter4.java
@@ -163,3 +163,14 @@ public class Main{
     }
   }
 }
+
+4-15 実行結果は？
+public class Main {
+  public static void main(String[] args){
+    int[] arrayA = {1, 2, 3};
+    int[] arrayB;
+    arrayB = arrayA;
+    arrayB[0] = 100;
+    System.out.println(arrayA[0]);
+  }
+}
