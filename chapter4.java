843bb87c066b129a73d9c0b8ed0431b749bc2966
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-10 21:57:08 +0900

    [start]4-6:配列の後片付け

--- chapter4.java
+++ chapter4.java
@@ -174,3 +174,13 @@ public class Main {
     System.out.println(arrayA[0]);
   }
 }
+
+4-16 ガベージコレクション
+public class Main {
+  public static void main(String[] args){
+    boolean judge = true;
+    if (judge == true){
+      int[] array = {1, 2, 3};
+    }
+  }
+}
