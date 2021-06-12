426c844c648bbd2ccdb67ac1ac6c980a22cc9f11
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-12 22:46:12 +0900

    練習問題4-2

--- chapter4.java
+++ chapter4.java
@@ -220,3 +220,16 @@ public class Main{
     String[] names = new String[3];
   }
 }
+
+4-2
+public class Main{
+  public static void main(String[] args){
+    int[] moneyList = {121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++){
+      System.out.println(moneyList[i]);
+    }
+    for (int m : moneyList){
+      System.out.println(m);
+    }
+  }
+}
