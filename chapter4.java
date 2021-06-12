56b061988fe0aeaaf4759f145b8b60b7dc6e5d43
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-12 21:53:32 +0900

    4-19

--- chapter4.java
+++ chapter4.java
@@ -200,3 +200,12 @@ public class Main{
     int[][] scores = new in
   }
 }
+
+4-19 親配列と子配列の要素数を表示
+public class Main {
+  public static void main(String[] args){
+    int[][] scores = {{40, 50, 60}, {80, 60, 70}};
+    System.out.println(scores.length);
+    System.out.println(scores[0].length);
+  }
+}
