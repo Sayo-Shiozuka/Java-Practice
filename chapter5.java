6488344e2f8b4348a8ac3b84a5e2d1b97330ffab
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-13 22:35:16 +0900

    5-4:引数の例(渡す値が複数の場合)

--- chapter5.java
+++ chapter5.java
@@ -30,3 +30,17 @@ public class Main {
     methodA();
   }
 }
+
+//5-4 引数の例(渡す値が1つの場合)
+public class Main {
+  public static void main(String[] args){
+    System.out.println("メソッドを呼び出します")
+    hello("湊");
+    hello("朝香");
+    hello("菅原");
+    System.out.println("メソッドの呼び出しが終わりました")
+  }
+  public static void hello(String name){
+    System.out.println(name + "さん、こんにちは");
+  }
+}
