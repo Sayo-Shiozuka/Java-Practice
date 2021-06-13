6787d9d1be5d3f4e1f65aeea6fcc325df8d55c9c
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-13 22:40:29 +0900

    5-5:引数の例(渡す値が複数の場合)

--- chapter5.java
+++ chapter5.java
@@ -44,3 +44,16 @@ public class Main {
     System.out.println(name + "さん、こんにちは");
   }
 }
+
+//5-5 引数の例(渡す値が複数の場合)
+public class Main{
+  public static void main(String[] args){
+    add(100, 20);
+    add(200, 50);
+  }
+  //複数の値を受け取るaddメソッド
+  public static void add(int x, int y){
+    int ans = x + y;
+    System.out.println(x + "y" + y + "=" + ans);
+  }
+}
