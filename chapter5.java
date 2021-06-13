53e329683d223c898e9c5197aa647162033ce4ab
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-13 22:22:12 +0900

    5-2:メソッドの呼び出し

--- chapter5.java
+++ chapter5.java
@@ -4,3 +4,15 @@ public class Main{
     System.out.println("湊さん、こんにちは");
   }
 }
+
+// 5-2 メソッドの呼び出し
+public class Main {
+  public static void main(String[] args){
+    System.out.println("メソッドを呼び出します");
+    hello();
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void hello() {
+    System.out.println("湊さん、こんにちは");
+  }
+}
