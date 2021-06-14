7cb828a867bb6fb3f021949d09c966629b4d0c34
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-14 22:08:41 +0900

    5-7:戻り値の例

--- chapter5.java
+++ chapter5.java
@@ -71,3 +71,15 @@ public class Main {
     System.out.println(x + "+" + y + "=" + ans);
   }
 }
+
+//5-7 戻り値の例
+public class Main {
+  public static int add(int x, int y) {
+    int ans = x + y;
+    return ans;
+  }
+  public static void main(String[] args){
+    int ans = add(100, 10);
+    System.out,println("100 + 10 = " + ans);
+  }
+}
