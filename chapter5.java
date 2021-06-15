809d0b3c80a0f218512b80a20b20a83155746314
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-15 22:59:00 +0900

    5-10:オーバーロード(引数の数が異なる場合)

--- chapter5.java
+++ chapter5.java
@@ -122,3 +122,17 @@ public class Main {
     System.out.println(add("Hello", "World"));う
   }
 }
+
+//5-11 オーバーロード(引数の数が異なる場合)
+public class Main {
+  public static int add(int x, int y) {
+    return x + y;
+  }
+  public static int add(int x, int y, int z) {
+    return x + y + z;
+  }
+  public static void main(String[] args) {
+    System.out.println("10+20=" + add(10, 20));
+    System.out.println("10+20+30=" + add(10, 20, 30));
+  }
+}
