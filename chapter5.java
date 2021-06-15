da685e6f96f3e24180ecffc5028dee1546905676
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-15 23:02:32 +0900

    5-11:引数に配列を受け取るメソッドの利用

--- chapter5.java
+++ chapter5.java
@@ -136,3 +136,17 @@ public class Main {
     System.out.println("10+20+30=" + add(10, 20, 30));
   }
 }
+
+//5-12 引数に配列を受け取るメソッドの利用
+public class Main {
+  //int型配列を受け取り、すべての要素を表示するメソッド
+  public static void printArray(int[] array){
+    for (int element : array){
+      System.out.println(element)
+    }
+  }
+  public static void main(String[] args) {
+    int[] array = {1, 2, 3}:
+    printArray(array);   //配列を渡す
+  }
+}
