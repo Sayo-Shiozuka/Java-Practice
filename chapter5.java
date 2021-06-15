01e00430f17e9cab2d829c51e60e8ef102c3c011
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-15 22:52:18 +0900

    5-10:オーバーロード(引数の型が異なる場合

--- chapter5.java
+++ chapter5.java
@@ -101,3 +101,24 @@ public static int sample() {
   return 1;
   int x = 10; //この文が実行されることはないのでコンパイルエラーになる
 }
+
+//5-10 オーバーロード(引数の型が異なる場合)
+public class Main {
+  //1つ目のaddメソッド
+  public static int add(int x, int y) {
+    return x + y;
+  }
+  //2つ目のaddメソッド
+  public static double add(double x, double y){
+    return x + y;
+  }
+  //3つ目のaddメソッド
+  public static String add(String x, String y) {
+    return x + y;
+  }
+  public static void main(String[] args){
+    System.out.println(add(10, 20));
+    System.out.println(add(3.5, 2.7));
+    System.out.println(add("Hello", "World"));う
+  }
+}
