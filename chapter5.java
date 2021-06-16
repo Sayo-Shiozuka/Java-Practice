3f95f49c13694ca0fbbddb3477fa1e3a4720c5c9
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-16 23:03:16 +0900

    5-13:同じ配列を参照していることを確認する

--- chapter5.java
+++ chapter5.java
@@ -150,3 +150,21 @@ public class Main {
     printArray(array);   //配列を渡す
   }
 }
+
+//5-13 同じ配列を3章していることを確認する
+public class Main {
+  // int型配列を受け取り、
+  //配列内の要素すべてに1を加えるメソッド
+  public static void incArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      array[i]++;
+    }
+  }
+  public static void main(String[] args) {
+    int[] array = {1, 2, 3};
+    incArray(array);
+    for (int i : array) {
+      System.out.println(i);
+    }
+  }
+}
