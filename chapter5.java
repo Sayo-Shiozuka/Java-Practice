16de0cf76dbb5b953baa3b9f103346dd72e234b5
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-16 23:08:42 +0900

    5-14:戻り値が配列の場合

--- chapter5.java
+++ chapter5.java
@@ -168,3 +168,20 @@ public class Main {
     }
   }
 }
+
+//5-14 戻り値に配列を用いる
+public class Main {
+  public static int[] makeArray(int size) {
+    int[] newArray = new int[size];
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] = i;
+    }
+    return newArray;
+  }
+  public static void main(String[] args) {
+    int[] array = makeArray(3);
+    for (int i : array) {
+      System.out.println(i);
+    }
+  }
+}
