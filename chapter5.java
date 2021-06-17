55a76703bf11bdbf8a461f28bbaf43f3fae9cc86
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-17 22:51:10 +0900

    5-3:練習問題

--- chapter5.java
+++ chapter5.java
@@ -219,3 +219,22 @@ public class Main {
     System.out.println("本文：" + text);
   }
 }
+
+//5-3
+public class Main {
+  public static void main(String[] args) {
+    String address = "uso800@xxx.com";
+    String text = "今度、飲みに行きませんか";
+    email(address, text)
+  }
+  public static void email(String address, String text) {
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名：無題");
+    System.out.println("本文：" + text);
+  }
+  public static void email(String title, String address, String text) {
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名：" + title);
+    System.out.println("本文：" + text); 
+  }
+}
