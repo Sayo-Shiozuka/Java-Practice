f6f8ba8631f67c8719ad6ce7a561f956cefdc254
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-17 22:42:33 +0900

    5-2:練習問題

--- chapter5.java
+++ chapter5.java
@@ -204,3 +204,18 @@ public class Main {
     System.out.println("十二支は" + zodiac + "です");
   }
 }
+
+//5-2
+public class Main {
+  public static void main(String[] args) {
+    String title = "お誘い";
+    String address = "uso800@xxx.com";
+    String text = "今度、飲みに行きませんか";
+    email(title, address, text);
+  }
+  public static void email(String title, String address, String text) {
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名：" + title);
+    System.out.println("本文：" + text);
+  }
+}
