7358aaba4feb496ea138b0034dfc73007a1cc02c
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-19 23:27:14 +0900

    6-2:CalcLogic.javaに計算処理を追加する

--- chapter6.java
+++ chapter6.java
@@ -13,3 +13,13 @@ public class Calc {
     return (a - b);
   }
 }
+
+//6-2 CalcLogic.javaに計算処理を追加する
+public class CalcLogic {
+  public static int tasu(int a, int b) {
+    return (a + b);
+  }
+  public static int hiku(int a, int b) {
+    return (a - b);
+  }
+}
