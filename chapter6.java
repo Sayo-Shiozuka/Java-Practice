e300f9a235e2dee7d8083bb45dfeb899d339b706
Sayo Fukuyama <sayosf1026@gmail.com>
2021-06-19 23:39:14 +0900

    6-4:正しく修正したCalc.java

--- chapter6.java
+++ chapter6.java
@@ -33,3 +33,13 @@ public class Calc {
     System.out.println("足すと" + total + "、引くと" + delta);
   } 
 }
+
+//6-4　正しく修正した Calc.java
+public class Calc {
+  public static void main(String[] args) {
+    int a = 10; int b = 2;
+    int total = CalcLogic.tasu(a, b);
+    int delta = CalcLogic.hiku(a, b);
+    System.out.println("足すと" + total + "、引くと" + delta);
+  }
+}
